3a168d90d0812f04a3abb17ce68eec4378104b76
Yasmine <yasmine.mankour.sio@gmail.com>
2025-06-30 10:30:03 +0200

    feat

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -7,22 +7,48 @@ const Skills = () => {
       <div className="skills-box">
         <h2>Développement</h2>
         <ul>
+          <li>
+            <h3>Front end :</h3>
+          </li>
           <li>HTML</li>
           <li>CSS</li>
           <li>JS</li>
-          <li>REACT</li>
-          <li>ANGULAR</li>
+          <li>
+            <h3>Back end :</h3>
+          </li>
+          <li>STAPI</li>
           <li>PYTHON</li>
-          <li>NODE JS</li>
-          <li>SQL</li>
+          <li>JAVA</li>
+          <li>
+            <h3>Framework :</h3>
+          </li>
+          <li>ANGULAR</li>
+          <li>REACT</li>
+          <li>
+            <h3>Base de donnée :</h3>
+          </li>
+          <li>MYSQL</li>
           <li>POSTGRESQL</li>
           <li>MONGODB</li>
+          <li>
+            <h3>Autres :</h3>
+          </li>
+          <li>UML</li>
+          <li>DOCKER</li>
+          <li>IT NETWORK SETUP</li>
         </ul>
       </div>
 
       <div className="skills-box">
         <h2>Outils</h2>
         <ul>
+          <li>VISUAL STUDIO CODE</li>
+          <li>JUPYTER NOTEBOOK</li>
+          <li>VIRTUAL BOX</li>
+          <li>PACKET TRACER</li>
+          <li>TRELLO</li>
+          <li>ODOO</li>
+          <li>GLPI</li>
           <li>PHOTOSHOP</li>
           <li>FIGMA</li>
           <li>PROCREATE</li>
