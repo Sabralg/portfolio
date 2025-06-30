a9409129cd610697c0a24007d5b691488a955266
Yasmine <yasmine.mankour.sio@gmail.com>
2025-06-30 10:35:42 +0200

    fix timeline

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,31 +1,39 @@
 import React from "react";
-import './Timeline.css';
+import "./Timeline.css";
 
 const timelineData = [
-    { year: 2025, left: "Stage Développeuse Web", right: "BUT3 Info", pin: true  },
-    { year: 2024, left: "Stage Développeuse Full Stack", right: "DUT Technologie de l'information" },
-    { year: 2023, left: "Technicienne Informatique", right: "" },
-    { year: 2022, left: "Équipière Polyvalente", right: "BTS Services Informatiques aux Organisations" },
-    { year: 2020, left: "Baccalauréat Scientifique, Science de l'Ingénieur" },
+  { year: 2025, left: "Stage Développeuse Web", right: "BUT3 Info", pin: true },
+  {
+    year: 2024,
+    left: "Stage Développeuse Full Stack",
+    right: "DUT Technologie de l'information",
+  },
+  { year: 2023, left: "Technicienne Informatique", right: "" },
+  {
+    year: 2022,
+    left: "Équipière Polyvalente",
+    right: "BTS Services Informatiques aux Organisations",
+  },
+  { year: 2020, right: "Baccalauréat Scientifique, Science de l'Ingénieur" },
 ];
 
 const Timeline = () => {
-    return (
-        <div className="timeline-container">
-            {timelineData.map((item, index) => (
-                <div key={index} className="timeline-item">
-                    <div className="timeline-left">{item.left}</div>
-                    <div className="timeline-middle">
-                    <div className="timeline-circle">
-                        {item.pin && <span className="pin">📍</span>}
-                    </div>
-                    <div className="timeline-year">{item.year}</div>
-                    </div>
-                    <div className="timeline-right">{item.right}</div>
-                </div>
-            ))}
+  return (
+    <div className="timeline-container">
+      {timelineData.map((item, index) => (
+        <div key={index} className="timeline-item">
+          <div className="timeline-left">{item.left}</div>
+          <div className="timeline-middle">
+            <div className="timeline-circle">
+              {item.pin && <span className="pin">📍</span>}
+            </div>
+            <div className="timeline-year">{item.year}</div>
+          </div>
+          <div className="timeline-right">{item.right}</div>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
-    
+
 export default Timeline;
